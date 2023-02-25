688633e0078cafe684ed78005d585e37b37f7343
Nikolay <labazopelastra@gmail.com>
2023-02-25 19:22:52 +0700

    add exam12

---
+++ Exam12.py
@@ -0,0 +1,24 @@
+import random
+# Петя и Катя – брат и сестра. 
+# Петя – студент, а Катя – школьница. 
+# Петя помогает Кате по математике. 
+# Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. 
+# Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
+# Помогите Кате отгадать задуманные Петей числа.
+
+
+summa=int(input("Введите сумму чисел: "))
+multy=int(input("Введите произведение чисел: "))
+
+for firstNumber in range(summa):
+    for secondNumber in range(multy):
+        if (firstNumber+secondNumber)==summa and firstNumber*secondNumber==multy:
+            print(firstNumber,secondNumber) 
+
+
+
+
+
+
+
+
